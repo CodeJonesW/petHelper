b30e259c5259c5a1940da64063ad4b2c5b93f7e0
CodeJonesW <williamjonescodes@gmail.com>
2020-12-29 12:10:38 -0500

    initial petHelper creation implemented

--- public/app.js
+++ public/app.js
@@ -14,7 +14,7 @@ const provider = new firebase.auth.GoogleAuthProvider();
 signInButton.onclick = () => auth.signInWithPopup(provider);
 signOutBtn.onclick = () => auth.signOut()
 
-
+// conditional logic for when auth state changes allows us to control ui pending the state of login
 auth.onAuthStateChanged(user => {
     if (user) {
         console.log("User: ", user)
@@ -22,69 +22,93 @@ auth.onAuthStateChanged(user => {
         whenSignedIn.hidden = false;
         whenSignedOut.hidden = true;
         signOutBtn.hidden = false
-        userDetails.innerHTML = `<h3> Hi ${user.displayName}! <p>Please feel free to record your thoughts in this space </p>`;
+        userDetails.innerHTML = `<h3> Hi ${user.displayName}! <p>Find pet care in your area!  </p>`;
 
     } else {
         whenSignedIn.hidden = true;
         whenSignedOut.hidden = false;
         userDetails.innerHTML = '';
-        noteDiv.hidden = true;
-        noteList.innerHTML = '';
+        petHelperList.hidden = true;
+        petHelperList.innerHTML = '';
     }
 });
 
 
 
 // // manage notes via firestore
-// const db = firebase.firestore();
-
-// const createNote = document.getElementById('createNote')
-// const noteList = document.getElementById('notesList')
-// const noteDiv = document.getElementById('noteDiv')
-// const noteInput = document.getElementById('noteInput')
-// let notesRef;
-// let unsubscribe;
+const db = firebase.firestore();
 
 
-// auth.onAuthStateChanged(user => {
-//     if (user) {
+const petHelperList = document.getElementById('petHelperList')
+const petHelperDiv = document.getElementById('petHelperDiv')
+const createPetHelper = document.getElementById('createPetHelper')
+const firstNameInput = document.getElementById('firstName')
+const lastNameInput = document.getElementById('lastName')
 
-//         const { serverTimestamp } = firebase.firestore.FieldValue;
+// two things i need when accessing firestore in real time
+// 1) reference to document or collection i want access such
+//    as petHelperRef. the reference is starting place for CRUD actions 
+// 2) front end needs to react to changes on server so we are subcribed to a stream of changes on DB
+//    we will need to stop listening/unsubscribe to this stream to avoid memory leaks and extra cost$$$
+//    by reading and listening to docs we dont need for ui
 
-//         noteDiv.hidden = false
-//         notesRef = db.collection('notes')
-//         createNote.onclick = () => {
-//             // console.log("my note", noteInput.value)
-//             if (noteInput.value.trim() === "") {
-//                 alert("Please enter a note!");
-//                 noteInput.focus();
-//                 return false;
-//             } else {
-//                 notesRef.add({
-//                     uid: user.uid,
-//                     descripton: noteInput.value,
-//                     // use servertimeStamp instead of Date.now() so that date obj is consistent across all client devices
-//                     createdAt: serverTimestamp()
-//                 });
-//             }
+let petHelperRef;
+let unsubscribe; 
 
-//         }
-//         unsubscribe = notesRef
-//             .where('uid', '==', user.uid)
-//             .orderBy('createdAt')
-//             .onSnapshot(querySnapshot => {
-//                 const items = querySnapshot.docs.map(doc => {
-//                     // let date = doc.data().createdAt.toDate()
+// we want our db records to only be accessible to logged in users so we need to check
+// auth state before making a call to the Db
+auth.onAuthStateChanged(user => {
+    if (user) {
 
-//                     return `<li>${doc.data().descripton}</li>`
-//                 });
-//                 noteList.innerHTML = items.join('');
-//             })
-//     } else {
+        const { serverTimestamp } = firebase.firestore.FieldValue;
 
-//         unsubscribe && unsubscribe();
-//     }
-// });
+        petHelperDiv.hidden = false
+        petHelperRef = db.collection('petHelpers')
+
+
+        createPetHelper.onclick = (e) => {
+            console.log(e)
+            // e.preventDefault()
+            // console.log("my note", noteInput.value)
+            if (firstNameInput.value.trim() === "" || lastNameInput.value.trim() === "" ) {
+                alert("Please provide a full name");
+                firstNameInput.focus(); 
+                return false;
+            } else {
+                petHelperRef.add({
+                    uid: user.uid,
+                    firstName: firstNameInput.value,
+                    lastName: lastNameInput.value,
+                    providedServices: ["walk"],
+                    // use servertimeStamp instead of Date.now() so that date obj is consistent across all client devices
+                    createdAt: serverTimestamp()
+                });
+            }
+
+        }
+
+        // main advantage of firestore is listening to changes in realtime by making a query
+        // querys return a function that you can use to unsubscribe from the query later on
+        // use it when the app state changes like when user logs out
+        unsubscribe = petHelperRef
+        // grabs only documents with matching ids
+            // .where('uid', '==', user.uid)
+            // grab data one time .get if you want to subcribe to the stream of changes use onSnapshot
+            .onSnapshot(querySnapshot => {
+                // this is the callback function that runs on when changes happen on the server 
+                // querySnapshot is the new data we can now use to update the ui
+                const items = querySnapshot.docs.map(doc => {
+                    // let date = doc.data().createdAt.toDate()
+
+                    return `<li>${doc.data().firstName}</li>`
+                });
+                petHelperList.innerHTML = items.join('');
+            })
+    } else {
+
+        unsubscribe && unsubscribe();
+    }
+});
 
 
 
